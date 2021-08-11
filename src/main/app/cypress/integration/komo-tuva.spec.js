5ea12ebd80fa7e100ceccd00e857f4adb0db3ad4
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2021-08-11 12:19:20 +0300

    KTO-1303: Lisätään testit TUVA KOMO:lle konfossa

--- src/main/app/cypress/integration/komo-tuva.spec.js
+++ src/main/app/cypress/integration/komo-tuva.spec.js
@@ -2,19 +2,38 @@ import { playMocks } from 'kto-ui-common/cypress/mockUtils';
 
 import komoTuvaMocks from '#/cypress/mocks/komo-tuva.mocks.json';
 
-describe('Osaamisala KOMO', () => {
+describe('TUVA KOMO', () => {
   beforeEach(() => {
     playMocks(komoTuvaMocks);
-  });
-  it('TUVA KOMO renders properly', () => {
     cy.visit('/fi/koulutus/1.2.246.562.13.00000000000000000623');
 
     // Wait for everything to load
     cy.findByRole('progressbar').should('not.exist');
+  });
+
+  it('renders perustiedot with koulutustyyppi and opintojenlaajuus', () => {
     cy.get('h1').contains('Tutkintokoulutukseen valmentava koulutus (TUVA)');
     cy.findByTestId('koulutustyyppi').contains(
       'Tutkintokoulutukseen valmentava koulutus'
     );
     cy.findByTestId('opintojenLaajuus').contains('38 viikkoa');
+  });
+
+  it('renders kuvaus without a link to ePerusteet', () => {
+    cy.findByTestId('kuvaus').within(() => {
+      cy.get('h2').contains('Koulutuksen kuvaus');
+      cy.findByText('Tämä on kuvaus fi.');
+      cy.findByTestId('eperuste-linkki').should('not.exist');
+    });
+  });
+
+  it('renders kuvaus with a link to ePerusteet', () => {
+    cy.visit('/fi/koulutus/1.2.246.562.13.00000000000000000624');
+
+    // Wait for everything to load
+    cy.findByRole('progressbar').should('not.exist');
+    cy.findByTestId('kuvaus').within(() => {
+      cy.findByTestId('eperuste-linkki').should('exist');
+    });
   });
 });
