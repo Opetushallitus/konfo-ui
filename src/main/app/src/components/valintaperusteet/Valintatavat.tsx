1c20d520e78512435c46f680c32f413bf02efb51
Tatu Honkanen <tatu.honkanen@cybercom.com>
2021-08-23 16:07:54 +0300

    OY-3035 : Change breaking grid to box

--- src/main/app/src/components/valintaperusteet/Valintatavat.tsx
+++ src/main/app/src/components/valintaperusteet/Valintatavat.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import { Box, Divider, Grid, Typography } from '@material-ui/core';
+import {Box, Divider, Grid, Typography} from '@material-ui/core';
 import _ from 'lodash';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
-import { LocalizedHTML } from '#/src/components/common/LocalizedHTML';
-import { localize } from '#/src/tools/localization';
-import { toId } from '#/src/tools/utils';
-import { Translateable } from '#/src/types/common';
+import {LocalizedHTML} from '#/src/components/common/LocalizedHTML';
+import {localize} from '#/src/tools/localization';
+import {toId} from '#/src/tools/utils';
+import {Translateable} from '#/src/types/common';
 
-import { SisaltoComponent } from './Sisalto';
-import { Valintatapa } from './ValintaperusteTypes';
+import {SisaltoComponent} from './Sisalto';
+import {Valintatapa} from './ValintaperusteTypes';
 
 type Props = { hakukohteenKynnysehto: Translateable; valintatavat: Array<Valintatapa> };
 
@@ -32,31 +32,29 @@ export const Valintatavat = ({ hakukohteenKynnysehto, valintatavat = [] }: Props
               <Typography variant="h3">{localize(nimi)}</Typography>
             </Box>
             {vahimmaispisteet && (
-              <Grid
-                container
-                item
-                direction="row"
-                xs={8}
+              <Box
+                display="grid"
+                gridTemplateColumns="repeat(3, 1fr)"
                 style={{ paddingBottom: '8px' }}>
-                <Grid item container direction="column" xs={12} sm={6}>
+                <Box gridColumn="1">
                   <Typography variant="h5">
                     {t('valintaperuste.vahimmaispisteet')}
                   </Typography>
                   <Typography variant="body1" component="div">
                     {vahimmaispisteet}
                   </Typography>
-                </Grid>
+                </Box>
                 {enimmaispisteet && (
-                  <Grid item container direction="column" xs={12} sm={6}>
+                  <Box gridColumn="2">
                     <Typography variant="h5">
                       {t('valintaperuste.enimmaispisteet')}
                     </Typography>
                     <Typography variant="body1" component="div">
                       {enimmaispisteet}
                     </Typography>
-                  </Grid>
+                  </Box>
                 )}
-              </Grid>
+              </Box>
             )}
             {!_.isEmpty(kynnysehto) && (
               <Box py={1}>
