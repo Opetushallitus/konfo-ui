0b88f7b466dc5a2c98fd0ae3a2b0b875fd45375f
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-10-20 15:47:13 +0300

    Siivottu pois custom hookkiin refaktoroitua redux-koodia

--- src/main/app/src/store/reducers/koulutusSlice.js
+++ src/main/app/src/store/reducers/koulutusSlice.js
@@ -2,47 +2,24 @@ import { createSlice } from '@reduxjs/toolkit';
 import _ from 'lodash';
 import _fp from 'lodash/fp';
 
-import {
-  getKoulutus,
-  getKoulutusKuvaus,
-  getKoulutusJarjestajat,
-  getSuositellutKoulutukset,
-  getEperusteKuvaus,
-} from '#/src/api/konfoApi';
-import { KOULUTUS_TYYPPI } from '#/src/constants';
+import { getKoulutusJarjestajat } from '#/src/api/konfoApi';
 
 const IDLE_STATUS = 'idle';
 const LOADING_STATUS = 'loading';
 
 export const initialState = {
-  koulutusStatus: IDLE_STATUS,
   jarjestajatStatus: IDLE_STATUS,
   tulevatJarjestajatStatus: IDLE_STATUS,
-  suositellutKoulutuksetStatus: IDLE_STATUS,
-  koulutukset: {},
   jarjestajat: [],
   jarjestajatFilters: {},
   tulevatJarjestajat: {},
-  suositellutKoulutukset: {},
-  koulutusError: null,
   jarjestajatError: null,
-  suositellutKoulutuksetError: null,
 };
 
 const koulutusSlice = createSlice({
   name: 'koulutus',
   initialState,
   reducers: {
-    fetchKoulutusStart(state) {
-      if (state.koulutusStatus === IDLE_STATUS) {
-        state.koulutusStatus = LOADING_STATUS;
-      }
-    },
-    fetchSuositellutKoulutuksetStart(state) {
-      if (state.suositellutKoulutuksetStatus === IDLE_STATUS) {
-        state.suositellutKoulutuksetStatus = LOADING_STATUS;
-      }
-    },
     fetchJarjestajatStart(state) {
       if (state.jarjestajatStatus === IDLE_STATUS) {
         state.jarjestajatStatus = LOADING_STATUS;
@@ -51,21 +28,6 @@ const koulutusSlice = createSlice({
     fetchTulevatJarjestajatStart(state) {
       if (state.tulevatJarjestajatStatus === IDLE_STATUS) {
         state.tulevatJarjestajatStatus = LOADING_STATUS;
-      }
-    },
-    fetchKoulutusSuccess(state, { payload }) {
-      if (state.koulutusStatus === LOADING_STATUS) {
-        const { koulutus, oid } = payload;
-        state.koulutukset[oid] = koulutus;
-        state.error = null;
-        state.koulutusStatus = IDLE_STATUS;
-      }
-    },
-    fetchSuositellutKoulutuksetSuccess(state, { payload }) {
-      if (state.suositellutKoulutuksetStatus === LOADING_STATUS) {
-        state.suositellutKoulutukset = payload.suositellutKoulutuksetData;
-        state.suositellutKoulutuksetError = null;
-        state.suositellutKoulutuksetStatus = IDLE_STATUS;
       }
     },
     fetchJarjestajatSuccess(state, { payload }) {
@@ -97,12 +59,6 @@ const koulutusSlice = createSlice({
         state.tulevatJarjestajatStatus = IDLE_STATUS;
       }
     },
-    fetchKoulutusError(state, action) {
-      if (state.koulutusStatus === LOADING_STATUS) {
-        state.koulutusError = action.payload;
-        state.koulutusStatus = IDLE_STATUS;
-      }
-    },
     fetchSuositellutKoulutuksetError(state, action) {
       if (state.suositellutKoulutuksetStatus === LOADING_STATUS) {
         state.suositellutKoulutuksetError = action.payload;
@@ -113,74 +69,15 @@ const koulutusSlice = createSlice({
 });
 
 export const {
-  fetchKoulutusStart,
-  fetchSuositellutKoulutuksetStart,
   fetchJarjestajatStart,
   fetchTulevatJarjestajatStart,
-  fetchKoulutusSuccess,
-  fetchSuositellutKoulutuksetSuccess,
   fetchJarjestajatSuccess,
   fetchTulevatJarjestajatSuccess,
-  fetchKoulutusError,
   fetchSuositellutKoulutuksetError,
   fetchJarjestajatError,
   fetchTulevatJarjestajatError,
 } = koulutusSlice.actions;
 export default koulutusSlice.reducer;
-
-export const fetchKoulutus = (oid, draft) => async (dispatch) => {
-  try {
-    dispatch(fetchKoulutusStart());
-    const koulutusData = await getKoulutus(oid, draft);
-    if (
-      (koulutusData?.koulutustyyppi === KOULUTUS_TYYPPI.AMM && koulutusData.ePerusteId) ||
-      (koulutusData?.koulutustyyppi === KOULUTUS_TYYPPI.AMM_OSAAMISALA &&
-        koulutusData.ePerusteId)
-    ) {
-      const koulutusKuvausData = await getKoulutusKuvaus(koulutusData.ePerusteId);
-      _.set(koulutusData, 'metadata.kuvaus', koulutusKuvausData);
-    } else if (koulutusData?.koulutustyyppi === KOULUTUS_TYYPPI.AMM_TUTKINNON_OSA) {
-      const tutkinnonOsat = koulutusData?.metadata?.tutkinnonOsat ?? [];
-      const eperusteet = _.uniq(tutkinnonOsat.map((t) => t.ePerusteId));
-
-      let e = [];
-      for (const index in eperusteet) {
-        const id = eperusteet[index];
-        const eperuste = await getEperusteKuvaus(id);
-        e.push(eperuste);
-      }
-
-      let yksikko = tutkinnonOsat[0]?.opintojenLaajuusyksikko;
-      let pisteet = tutkinnonOsat
-        .map((tutkinnonOsa) => tutkinnonOsa.opintojenLaajuusNumero)
-        .join(' + ');
-
-      _.set(koulutusData, 'metadata.opintojenLaajuusyksikko', yksikko);
-      _.set(koulutusData, 'metadata.opintojenLaajuus', {
-        nimi: {
-          sv: pisteet,
-          fi: pisteet,
-          en: pisteet,
-        },
-      });
-
-      _.set(koulutusData, 'eperusteet', e);
-    }
-    dispatch(fetchKoulutusSuccess({ oid, koulutus: koulutusData }));
-  } catch (err) {
-    dispatch(fetchKoulutusError(err.toString()));
-  }
-};
-
-export const fetchSuositellutKoulutukset = (oid) => async (dispatch) => {
-  try {
-    dispatch(fetchSuositellutKoulutuksetStart());
-    const suositellutKoulutuksetData = await getSuositellutKoulutukset({
-      koulutukset: oid,
-    });
-    dispatch(fetchSuositellutKoulutuksetSuccess({ oid, suositellutKoulutuksetData }));
-  } catch (error) {}
-};
 
 export const fetchKoulutusJarjestajat = (oid, requestParams) => async (dispatch) => {
   try {
@@ -217,51 +114,7 @@ export const fetchTulevatJarjestajat = (oid) => async (dispatch) => {
   }
 };
 
-export const fetchKoulutusWithRelatedData = (oid, draft) => {
-  return (dispatch) => {
-    Promise.all([
-      dispatch(fetchKoulutus(oid, draft)),
-      // NOTE: Suosittelu API is not available in any environment currently.
-      // TODO: Remove suosittelu code if it's not meant to be used anymore.
-      //dispatch(fetchSuositellutKoulutukset(oid)),
-      dispatch(fetchTulevatJarjestajat(oid)),
-    ]);
-  };
-};
-
-export const selectKoulutus = (oid) => (state) => {
-  const koulutusData = state.koulutus.koulutukset[oid];
-  if (koulutusData) {
-    return {
-      kuvaus: koulutusData.metadata?.kuvaus,
-      linkkiEPerusteisiin: koulutusData.metadata?.linkkiEPerusteisiin,
-      eperusteet: koulutusData.eperusteet,
-      ePerusteId: koulutusData?.ePerusteId,
-      tutkinnonOsat: koulutusData.metadata?.tutkinnonOsat,
-      tyotehtavatJoissaVoiToimia:
-        koulutusData.metadata?.kuvaus?.tyotehtavatJoissaVoiToimia,
-      suorittaneenOsaaminen: koulutusData.metadata?.kuvaus?.suorittaneenOsaaminen,
-      koulutusAla: koulutusData.metadata?.koulutusala,
-      tutkintoNimi: koulutusData?.nimi,
-      tutkintoNimikkeet: koulutusData.metadata?.tutkintonimike,
-      opintojenLaajuus: koulutusData.metadata?.opintojenLaajuus,
-      opintojenLaajuusNumero: koulutusData.metadata?.opintojenLaajuusNumero,
-      opintojenLaajuusyksikko: koulutusData.metadata?.opintojenLaajuusyksikko,
-      koulutusTyyppi: koulutusData.metadata?.tyyppi,
-      lisatiedot: koulutusData.metadata?.lisatiedot,
-      teemakuva: koulutusData?.teemakuva,
-      sorakuvaus: koulutusData?.sorakuvaus,
-    };
-  } else {
-    return undefined;
-  }
-};
-
-export const selectSuositellutKoulutukset = (state) =>
-  state.koulutus.suositellutKoulutukset;
-
 export const selectLoading = (state) =>
-  state.koulutus.koulutusStatus === LOADING_STATUS ||
   state.koulutus.tulevatJarjestajatStatus === LOADING_STATUS;
 
 export const selectTulevatJarjestajat = (state, oid) =>
