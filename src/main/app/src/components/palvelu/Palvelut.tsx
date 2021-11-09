d0a2fb4c1c35aef470b878108bf5836993115dd5
August Kilponen <august.kilponen@gmail.com>
2021-11-09 14:26:22 +0200

    Poistettu ylimääräiset otsikot tilanteessa jossa palvelu- tai ohje-kortit rivittyvät useammalle riville

--- src/main/app/src/components/palvelu/Palvelut.tsx
+++ src/main/app/src/components/palvelu/Palvelut.tsx
@@ -33,6 +33,7 @@ const useStyles = makeStyles({
   },
   rivi: {
     overflow: 'hidden',
+    paddingBottom: '24px',
   },
 });
 
@@ -43,9 +44,9 @@ const Rivi = ({ otsikko, rivit }: RiviProps) => {
 
   return (
     <>
+      { (rivit?.length > 0) ? ( <h1 className={classes.header}>{otsikko}</h1> ) : ( '' ) }
       {rivit.map((rivi) => (
         <Grid container className={classes.rivi} key={rivi.map((u) => u.id).join()}>
-          <h1 className={classes.header}>{otsikko}</h1>
           <Grid container spacing={3}>
             {rivi.map((p) => (
               <Palvelu id={p.id} key={p.id} />
