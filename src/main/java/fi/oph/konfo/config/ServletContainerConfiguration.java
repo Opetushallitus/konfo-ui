68bda64f92ec95307e9d3d89428fcb8e1d6d18da
Jussi Jartamo <jussi.jartamo@gofore.com>
2021-12-29 14:47:15 +0200

    fallback to tomcat with logback access log

--- src/main/java/fi/oph/konfo/config/ServletContainerConfiguration.java
+++ src/main/java/fi/oph/konfo/config/ServletContainerConfiguration.java
@@ -1,16 +1,15 @@
 package fi.oph.konfo.config;
 
-import ch.qos.logback.access.jetty.RequestLogImpl;
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.RequestLog;
-import org.eclipse.jetty.server.ServerConnector;
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.apache.catalina.connector.Connector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,41 +31,25 @@ public class ServletContainerConfiguration {
    *     servlet containerin konfigurointiin
    */
   @Bean
-  public ConfigurableServletWebServerFactory webServerFactory(
-      @Value("${konfo-ui.uses-ssl-proxy}") boolean sslProxy,
-      @Autowired UrlConfiguration configuration) {
-    JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
-    factory.addServerCustomizers(
-        new JettyServerCustomizer() {
-          @Override
-          public void customize(org.eclipse.jetty.server.Server server) {
-            if (sslProxy) {
-              HttpConfiguration http = new HttpConfiguration();
-              http.setSecureScheme("https");
-
-              ServerConnector connector = new ServerConnector(server);
-              connector.addConnectionFactory(new HttpConnectionFactory(http));
-
-              server.addConnector(connector);
-            }
-            server.setRequestLog(requestLog());
-          }
-
-          private RequestLog requestLog() {
-            RequestLogImpl requestLog = new RequestLogImpl();
-
-            String logbackAccess = configuration.getOrElse("logback.access", null);
-            if (logbackAccess != null) {
-              requestLog.setFileName(logbackAccess);
-            } else {
-              System.out.println(
-                  "JettyLauncher: Jetty access log is printed to console, use -Dlogback.access to set configuration file");
-              requestLog.setResource("/logback-access.xml");
-            }
-            requestLog.start();
-            return requestLog;
-          }
-        });
-    return factory;
+  @ConditionalOnProperty("konfo-ui.uses-ssl-proxy")
+  public WebServerFactoryCustomizer sslProxyCustomizer(@Autowired UrlConfiguration configuration) {
+    LOG.info("Enabling HTTPS Proxu configuration");
+    return (WebServerFactory container) -> {
+      if (container instanceof ConfigurableServletWebServerFactory) {
+        TomcatServletWebServerFactory tomcat = (TomcatServletWebServerFactory) container;
+        LogbackValve requestLog = new LogbackValve();
+        String logbackAccess = configuration.getOrElse("logback.access", null);
+        if (logbackAccess != null) {
+          requestLog.setFilename(logbackAccess);
+        }
+        tomcat.addContextValves(requestLog);
+        tomcat.addConnectorCustomizers(
+            (TomcatConnectorCustomizer)
+                (Connector connector) -> {
+                  connector.setScheme("https");
+                  connector.setSecure(true);
+                });
+      }
+    };
   }
 }
