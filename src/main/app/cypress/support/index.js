a94b843f1fbc9976974446087c370e6ce31612aa
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-02-01 14:25:14 +0200

    Refaktoroitu pois material-icons fontin käyttö suoraan fonts.googleapis.com:sta

--- src/main/app/cypress/support/index.js
+++ src/main/app/cypress/support/index.js
@@ -11,8 +11,5 @@ beforeEach(() => {
   cy.intercept('**/faq.e49945eb.svg', { fixture: 'faq.e49945eb.svg' });
   cy.intercept('**/ehoks.fdeaa517.svg', { fixture: 'ehoks.fdeaa517.svg' });
   cy.intercept('**/sv/translation.json', {});
-  cy.intercept('https://fonts.googleapis.com/icon?family=Material+Icons', {
-    fixture: 'material-icons.css',
-  });
   cy.setCookie('oph-mandatory-cookies-accepted', 'true'); //Hide cookie modal
 });
