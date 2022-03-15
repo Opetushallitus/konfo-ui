995d188733a6a947d791d849f0be37a98e4a9ff1
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-03-15 14:30:23 +0200

    OY-3399: Otettu chat käyttöön ja säädetty sen värimaailmaa

---
+++ src/main/app/src/useChat.ts
@@ -0,0 +1,29 @@
+import { useLayoutEffect } from 'react';
+import './chat.css';
+
+const CHAT_SCRIPT_ID = 'oc-start-up';
+
+export const useChat = () => {
+  useLayoutEffect(() => {
+    var script = document.createElement('script');
+    script.setAttribute('id', CHAT_SCRIPT_ID);
+    script.setAttribute('data-oc-language', 'fi_FI');
+    script.setAttribute(
+      'data-main',
+      'https://occhat.elisa.fi/chatserver//Scripts/oc-chat'
+    );
+    script.src = 'https://occhat.elisa.fi/chatserver//Scripts/oc-chat-v2.js';
+
+    if (document.domain === 'opintopolku.fi') {
+      // TODO: Aseta tuotannon chatin data-oc-service-attibuutti
+    } else {
+      // Testi-chat
+      script.setAttribute(
+        'data-oc-service',
+        '0e8747a9-e9c5-4988-bdfb-f52371da5eea-190-5C1C0B86B0B80C7F5F5B33ED4DB1B7A0AA5D2719'
+      );
+    }
+
+    document.head.appendChild(script);
+  }, []);
+};
