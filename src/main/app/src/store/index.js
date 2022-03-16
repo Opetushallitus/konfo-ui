c91838dd1431965b4fbea3e888d08df24edfa420
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-03-16 12:42:13 +0200

    Pientä koodin siivousta

--- src/main/app/src/store/index.js
+++ src/main/app/src/store/index.js
@@ -1,13 +1,10 @@
-import { getDefaultMiddleware, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 import reducer from './reducers';
-
-const middleware = [...getDefaultMiddleware()];
 
 export function getKonfoStore() {
   const store = configureStore({
     reducer,
-    middleware,
     devTools: process.env.NODE_ENV === 'development',
   });
 
