ca18eb7f18534b9f186153e123f53e82b9c8858f
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-03-21 11:46:29 +0200

    OY-3399: Näytetään chat vain kun valittu kieli on suomi

--- src/main/app/src/useChat.ts
+++ src/main/app/src/useChat.ts
@@ -1,7 +1,10 @@
 import { useEffect, useLayoutEffect, useState } from 'react';
+
 import './chat.css';
+import { useLanguageState } from './hooks';
 
 const CHAT_SCRIPT_ID = 'oc-start-up';
+const CHAT_WRAPPER_ID = 'ocChat-widget-container';
 
 const useDomQuery = (selector: string) => {
   const [el, setEl] = useState<Element | null>();
@@ -25,34 +28,45 @@ const useDomQuery = (selector: string) => {
 };
 
 export const useChat = () => {
-  useLayoutEffect(() => {
-    var script = document.createElement('script');
-    script.setAttribute('id', CHAT_SCRIPT_ID);
-    script.setAttribute('data-oc-language', 'fi_FI');
-    script.setAttribute(
-      'data-main',
-      'https://occhat.elisa.fi/chatserver//Scripts/oc-chat'
-    );
-    script.src = 'https://occhat.elisa.fi/chatserver//Scripts/oc-chat-v2.js';
+  const [language] = useLanguageState();
 
-    if (document.domain === 'opintopolku.fi') {
+  useLayoutEffect(() => {
+    if (language === 'fi') {
+      const script = document.createElement('script');
+      script.setAttribute('id', CHAT_SCRIPT_ID);
+      script.setAttribute('data-oc-language', 'fi_FI');
       script.setAttribute(
-        'data-oc-service',
-        '0e8747a9-e9c5-4988-bdfb-f52371da5eea-180-88756EB48B5E580FE8993DCDF914B33E8F2DA18D'
+        'data-main',
+        'https://occhat.elisa.fi/chatserver//Scripts/oc-chat'
       );
-    } else {
-      // Testi-chat
-      script.setAttribute(
-        'data-oc-service',
-        '0e8747a9-e9c5-4988-bdfb-f52371da5eea-190-5C1C0B86B0B80C7F5F5B33ED4DB1B7A0AA5D2719'
-      );
+      script.src = 'https://occhat.elisa.fi/chatserver//Scripts/oc-chat-v2.js';
+
+      if (document.domain === 'opintopolku.fi') {
+        script.setAttribute(
+          'data-oc-service',
+          '0e8747a9-e9c5-4988-bdfb-f52371da5eea-180-88756EB48B5E580FE8993DCDF914B33E8F2DA18D'
+        );
+      } else {
+        // Testi-chat
+        script.setAttribute(
+          'data-oc-service',
+          '0e8747a9-e9c5-4988-bdfb-f52371da5eea-190-5C1C0B86B0B80C7F5F5B33ED4DB1B7A0AA5D2719'
+        );
+      }
+
+      document.head.appendChild(script);
     }
 
-    document.head.appendChild(script);
-  }, []);
+    return () => {
+      const scriptEl = document.getElementById(CHAT_SCRIPT_ID);
+      const chatWrapperEl = document.getElementById(CHAT_WRAPPER_ID);
+      scriptEl?.remove();
+      chatWrapperEl?.remove();
+    };
+  }, [language]);
 
-  const chatStatusEl = useDomQuery('#oc-chat-status.hidden');
-  const chatWindowEl = useDomQuery('#oc-chat-window.hidden');
+  const chatStatusEl = useDomQuery('#oc-chat-status:not(.hidden)');
+  const chatWindowEl = useDomQuery('#oc-chat-window:not(.hidden)');
 
-  return !(chatStatusEl && chatWindowEl);
+  return Boolean(chatStatusEl || chatWindowEl);
 };
