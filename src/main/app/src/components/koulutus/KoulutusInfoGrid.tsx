e24503d27b6ac57810e5a10ddb6c6ea5002e49af
August Kilponen <august.kilponen@gmail.com>
2022-08-30 12:51:33 +0300

    Lisätty koulutukselle EQF ja NQF

--- src/main/app/src/components/koulutus/KoulutusInfoGrid.tsx
+++ src/main/app/src/components/koulutus/KoulutusInfoGrid.tsx
@@ -4,13 +4,15 @@ import { makeStyles } from '@material-ui/core';
 import ExtensionOutlinedIcon from '@material-ui/icons/ExtensionOutlined';
 import SchoolOutlinedIcon from '@material-ui/icons/SchoolOutlined';
 import TimelapseIcon from '@material-ui/icons/Timelapse';
+import LineStyleIcon from '@material-ui/icons/LineStyle';
 import { useTranslation } from 'react-i18next';
 
 import { InfoGrid } from '#/src/components/common/InfoGrid';
 import { Koulutustyyppi } from '#/src/constants';
 import { hasTutkintonimike } from '#/src/tools/hasTutkintonimike';
-import { localize } from '#/src/tools/localization';
-import { Translateable } from '#/src/types/common';
+import {localize, localizeArrayToCommaSeparated} from '#/src/tools/localization';
+import {Koodi, Translateable} from '#/src/types/common';
+import {ExternalLink} from "#/src/components/common/ExternalLink";
 
 const useStyles = makeStyles((theme) => ({
   koulutusInfoGridIcon: {
@@ -22,9 +24,11 @@ type Props = {
   nimikkeet: Array<Translateable>;
   koulutustyyppi?: Koulutustyyppi;
   laajuus: string;
+  eqf: Array<Koodi>;
+  nqf: Array<Koodi>;
 };
 
-export const KoulutusInfoGrid = ({ nimikkeet, koulutustyyppi, laajuus }: Props) => {
+export const KoulutusInfoGrid = ({ nimikkeet, koulutustyyppi, laajuus, eqf, nqf }: Props) => {
   const classes = useStyles();
   const { t } = useTranslation();
 
@@ -56,6 +60,19 @@ export const KoulutusInfoGrid = ({ nimikkeet, koulutustyyppi, laajuus }: Props) 
     text: laajuus,
     testid: 'opintojenLaajuus',
   });
+
+  const eqfString = localizeArrayToCommaSeparated(eqf) ? "EQF: " + localizeArrayToCommaSeparated(eqf, { sorted: true }) : undefined;
+  const nqfString = localizeArrayToCommaSeparated(nqf) ? "NQF: " + localizeArrayToCommaSeparated(nqf, { sorted: true }) : undefined;
+  const koulutuksenTasot = [eqfString, nqfString].filter(Boolean).join("\n");
+  if(koulutuksenTasot) {
+    perustiedotData.push({
+      icon: <LineStyleIcon className={classes.koulutusInfoGridIcon} />,
+      title: t('koulutus.koulutuksen-tasot'),
+      text: koulutuksenTasot,
+      testid: 'koulutuksenTasot',
+      modalText: <ExternalLink href={t('koulutus.koulutuksen-tasot-linkki.url')}>{t('koulutus.koulutuksen-tasot-linkki.teksti')}</ExternalLink>,
+    });
+  }
 
   return <InfoGrid gridData={perustiedotData} />;
 };
