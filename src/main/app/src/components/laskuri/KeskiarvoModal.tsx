b8a7ccc404154a2d4a2c300a7df742ee72fea807
Risto Salama <risto.salama@gofore.com>
2022-10-28 17:44:30 +0300

    Working on keskiarvolaskuri WIP

--- src/main/app/src/components/laskuri/KeskiarvoModal.tsx
+++ src/main/app/src/components/laskuri/KeskiarvoModal.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-import { Dialog, styled, Typography } from '@mui/material';
+import { Box, Dialog, styled, Typography } from '@mui/material';
+
+import { KeskiarvoLaskuri } from './KeskiarvoLaskuri';
+
+const PREFIX = 'KeskiarvoModal__';
+
+const classNames = {
+  container: `${PREFIX}container`,
+};
 
 const StyledDialog = styled(Dialog)(() => ({
-  padding: '1rem 2rem',
+  [`.${classNames.container}`]: {
+    backgroundColor: 'white',
+    padding: '2rem 1rem',
+  },
 }));
 
 type Props = {
@@ -14,12 +25,15 @@ type Props = {
 export const KeskiArvoModal = ({ open = false, closeFn }: Props) => {
   return (
     <StyledDialog open={open} onClose={closeFn}>
-      <Typography variant="h2">Hakupistelaskuri</Typography>
-      <Typography>
-        Arvioi peruskoulun päättötodistuksen keskiarvosi tai syötä kaikkien oppiaineiden
-        arvosanat. Laskuri laskee hakupisteesi yhteishakua varten uusimpien
-        valintaperusteiden mukaan.
-      </Typography>
+      <Box className={classNames.container}>
+        <Typography variant="h2">Hakupistelaskuri</Typography>
+        <Typography>
+          Arvioi peruskoulun päättötodistuksen keskiarvosi tai syötä kaikkien oppiaineiden
+          arvosanat. Laskuri laskee hakupisteesi yhteishakua varten uusimpien
+          valintaperusteiden mukaan.
+        </Typography>
+        <KeskiarvoLaskuri></KeskiarvoLaskuri>
+      </Box>
     </StyledDialog>
   );
 };
