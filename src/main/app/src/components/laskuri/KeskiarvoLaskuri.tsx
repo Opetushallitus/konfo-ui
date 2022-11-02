7cd2b4e29ed95a5c3771c20c641c5ae1385e50f0
Risto Salama <risto.salama@gofore.com>
2022-11-02 08:11:59 +0200

    Use localstorage to store given values to keskiarvot

--- src/main/app/src/components/laskuri/KeskiarvoLaskuri.tsx
+++ src/main/app/src/components/laskuri/KeskiarvoLaskuri.tsx
@@ -1,36 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Box, Typography, TextField } from '@mui/material';
 
 import { Keskiarvot } from './Keskiarvo';
+import { LocalStorageUtil } from './LocalStorageUtil';
+
+const AVERAGE_STORE_KEY = 'keskiarvot';
 
 type Props = {
   updateKeskiarvoToCalculate: (keskiarvo: Keskiarvot) => void;
 };
 
 export const KeskiarvoLaskuri = ({ updateKeskiarvoToCalculate }: Props) => {
-  const [keskiarvot, setKeskiarvot] = useState<Keskiarvot>({
-    lukuaineet: '',
-    taideTaitoAineet: '',
-    kaikki: '',
-  });
+  const [keskiarvot, setKeskiarvot] = useState<Keskiarvot | null>(null);
+
+  useEffect(() => {
+    if (keskiarvot != null) {
+      LocalStorageUtil.save(AVERAGE_STORE_KEY, keskiarvot);
+    }
+  }, [keskiarvot]);
+
+  useEffect(() => {
+    const savedResult = LocalStorageUtil.load(AVERAGE_STORE_KEY);
+    if (savedResult) {
+      setKeskiarvot(savedResult as Keskiarvot);
+      updateKeskiarvoToCalculate(savedResult as Keskiarvot);
+    } else {
+      setKeskiarvot({
+        lukuaineet: '',
+        taideTaitoAineet: '',
+        kaikki: '',
+      });
+    }
+  }, [updateKeskiarvoToCalculate]);
 
   const changeKeskiarvoLuku = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let newKeskiArvo = { ...keskiarvot };
+    let newKeskiArvo = { ...keskiarvot } as Keskiarvot;
     newKeskiArvo.lukuaineet = event.target.value;
     setKeskiarvot(newKeskiArvo);
     updateKeskiarvoToCalculate(newKeskiArvo);
   };
 
   const changeKeskiarvoTaide = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let newKeskiArvo = { ...keskiarvot };
+    let newKeskiArvo = { ...keskiarvot } as Keskiarvot;
     newKeskiArvo.taideTaitoAineet = event.target.value;
     setKeskiarvot(newKeskiArvo);
     updateKeskiarvoToCalculate(newKeskiArvo);
   };
 
   const changeKeskiarvoKaikki = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let newKeskiArvo = { ...keskiarvot };
+    let newKeskiArvo = { ...keskiarvot } as Keskiarvot;
     newKeskiArvo.kaikki = event.target.value;
     setKeskiarvot(newKeskiArvo);
     updateKeskiarvoToCalculate(newKeskiArvo);
@@ -46,17 +65,17 @@ export const KeskiarvoLaskuri = ({ updateKeskiarvoToCalculate }: Props) => {
         <TextField
           inputProps={{ inputMode: 'numeric' }}
           onChange={changeKeskiarvoLuku}
-          value={keskiarvot.lukuaineet}
+          value={keskiarvot?.lukuaineet}
           label="Lukuaineiden keskiarvo *"></TextField>
         <TextField
           inputProps={{ inputMode: 'numeric' }}
           onChange={changeKeskiarvoTaide}
-          value={keskiarvot.taideTaitoAineet}
+          value={keskiarvot?.taideTaitoAineet}
           label="Taide- ja taitoaineiden keskiarvo *"></TextField>
         <TextField
           inputProps={{ inputMode: 'numeric' }}
           onChange={changeKeskiarvoKaikki}
-          value={keskiarvot.kaikki}
+          value={keskiarvot?.kaikki}
           label="Kaikkien aineiden keskiarvo *"></TextField>
       </Box>
     </Box>
