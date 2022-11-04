bd7ef327413004fe6c40388de5ff9fdb10775097
Risto Salama <risto.salama@gofore.com>
2022-11-04 08:03:32 +0200

    Keskiarvolaskuri styling

--- src/main/app/src/components/laskuri/KeskiarvoLaskuri.tsx
+++ src/main/app/src/components/laskuri/KeskiarvoLaskuri.tsx
@@ -1,21 +1,58 @@
 import React, { useState, useEffect } from 'react';
 
-import { Box, Typography, TextField } from '@mui/material';
+import { Box, Typography, styled, Input, InputLabel, Grid } from '@mui/material';
+import _ from 'lodash';
+import { useTranslation } from 'react-i18next';
+import { colors } from 'src/colors';
 
+import { LabelTooltip } from '../common/LabelTooltip';
 import { Keskiarvot } from './Keskiarvo';
 import { LocalStorageUtil } from './LocalStorageUtil';
 
 const AVERAGE_STORE_KEY = 'keskiarvot';
 
+const PREFIX = 'keskiarvo__laskuri__';
+
+const classes = {
+  input: `${PREFIX}input`,
+  error: `${PREFIX}error`,
+};
+
+const LaskuriContainer = styled(Box)(() => ({
+  [`& .${classes.input}`]: {
+    border: `1px solid ${colors.lightGrey}`,
+    padding: '0 0.5rem',
+    '&:focus-within': {
+      borderColor: colors.black,
+    },
+    '&:hover': {
+      borderColor: colors.black,
+    },
+  },
+  [`& .${classes.error}`]: {
+    color: colors.red,
+    maxWidth: '60%',
+  },
+}));
+
 type Props = {
   updateKeskiarvoToCalculate: (keskiarvo: Keskiarvot) => void;
 };
 
+const keskiArvotIsEmpty = (kat: Keskiarvot) =>
+  _.matches(kat)({ lukuaineet: '', taideTaitoAineet: '', kaikki: '' });
+
 export const KeskiarvoLaskuri = ({ updateKeskiarvoToCalculate }: Props) => {
-  const [keskiarvot, setKeskiarvot] = useState<Keskiarvot | null>(null);
+  const { t } = useTranslation();
+
+  const [keskiarvot, setKeskiarvot] = useState<Keskiarvot>({
+    lukuaineet: '',
+    taideTaitoAineet: '',
+    kaikki: '',
+  });
 
   useEffect(() => {
-    if (keskiarvot != null) {
+    if (!keskiArvotIsEmpty(keskiarvot)) {
       LocalStorageUtil.save(AVERAGE_STORE_KEY, keskiarvot);
     }
   }, [keskiarvot]);
@@ -34,50 +71,91 @@ export const KeskiarvoLaskuri = ({ updateKeskiarvoToCalculate }: Props) => {
     }
   }, [updateKeskiarvoToCalculate]);
 
-  const changeKeskiarvoLuku = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let newKeskiArvo = { ...keskiarvot } as Keskiarvot;
-    newKeskiArvo.lukuaineet = event.target.value;
-    setKeskiarvot(newKeskiArvo);
-    updateKeskiarvoToCalculate(newKeskiArvo);
-  };
+  const isValidKeskiarvo = (ka: string) =>
+    '' === ka || (_.isNumber(Number(ka)) && Number(ka) >= 4 && Number(ka) <= 10); //TODO: comma
 
-  const changeKeskiarvoTaide = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const changeKeskiarvo = (event: React.ChangeEvent<HTMLInputElement>, avain: string) => {
+    const uusiArvo = event.target.value;
     let newKeskiArvo = { ...keskiarvot } as Keskiarvot;
-    newKeskiArvo.taideTaitoAineet = event.target.value;
+    newKeskiArvo[avain as keyof Keskiarvot] = uusiArvo;
     setKeskiarvot(newKeskiArvo);
-    updateKeskiarvoToCalculate(newKeskiArvo);
-  };
-
-  const changeKeskiarvoKaikki = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let newKeskiArvo = { ...keskiarvot } as Keskiarvot;
-    newKeskiArvo.kaikki = event.target.value;
-    setKeskiarvot(newKeskiArvo);
-    updateKeskiarvoToCalculate(newKeskiArvo);
+    if (isValidKeskiarvo(uusiArvo)) {
+      updateKeskiarvoToCalculate(newKeskiArvo);
+    }
   };
 
   return (
-    <Box>
-      <Typography variant="h3">Peruskoulun keskiarvot</Typography>
+    <LaskuriContainer>
+      <Typography variant="h3" sx={{ fontSize: '1.25rem' }}>
+        Peruskoulun keskiarvot
+      </Typography>
       <Typography>
         Jos haluat tarkan laskelman, kerro oppiaineiden arvosanat yksitellen.
       </Typography>
-      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-        <TextField
-          inputProps={{ inputMode: 'numeric' }}
-          onChange={changeKeskiarvoLuku}
-          value={keskiarvot?.lukuaineet}
-          label="Lukuaineiden keskiarvo *"></TextField>
-        <TextField
-          inputProps={{ inputMode: 'numeric' }}
-          onChange={changeKeskiarvoTaide}
-          value={keskiarvot?.taideTaitoAineet}
-          label="Taide- ja taitoaineiden keskiarvo *"></TextField>
-        <TextField
-          inputProps={{ inputMode: 'numeric' }}
-          onChange={changeKeskiarvoKaikki}
-          value={keskiarvot?.kaikki}
-          label="Kaikkien aineiden keskiarvo *"></TextField>
-      </Box>
-    </Box>
+      <Grid container justifyContent="space-evenly" columns={{ xs: 1, sm: 1, md: 3 }}>
+        <Grid item xs={1} sm={1} md={1}>
+          <InputLabel>
+            <Typography sx={{ fontWeight: 'bold' }}>Lukuaineiden keskiarvo *</Typography>
+            <Input
+              className={classes.input}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                changeKeskiarvo(event, 'lukuaineet')
+              }
+              value={keskiarvot?.lukuaineet}
+              error={!isValidKeskiarvo(keskiarvot?.lukuaineet)}
+              disableUnderline={true}></Input>
+          </InputLabel>
+          {!isValidKeskiarvo(keskiarvot?.lukuaineet) && (
+            <Typography variant="body2" className={classes.error}>
+              {t('pistelaskuri.error.keskiarvo')}
+            </Typography>
+          )}
+        </Grid>
+        <Grid
+          item
+          sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}
+          xs={1}
+          sm={1}
+          md={1}>
+          <InputLabel>
+            <Typography sx={{ fontWeight: 'bold' }}>
+              Taide- ja taitoaineiden keskiarvo *
+            </Typography>
+            <Input
+              className={classes.input}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                changeKeskiarvo(event, 'taideTaitoAineet')
+              }
+              value={keskiarvot?.taideTaitoAineet}
+              disableUnderline={true}></Input>
+          </InputLabel>
+          <LabelTooltip
+            title={t('pistelaskuri.taide-info')}
+            sx={{ marginLeft: '3px' }}></LabelTooltip>
+          {!isValidKeskiarvo(keskiarvot?.taideTaitoAineet) && (
+            <Typography variant="body2" className={classes.error}>
+              {t('pistelaskuri.error.keskiarvo')}
+            </Typography>
+          )}
+        </Grid>
+        <Grid item xs={1} sm={1} md={1}>
+          <InputLabel>
+            <Typography variant="subtitle1">Kaikkien aineiden keskiarvo *</Typography>
+            <Input
+              className={classes.input}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                changeKeskiarvo(event, 'kaikki')
+              }
+              value={keskiarvot?.kaikki}
+              disableUnderline={true}></Input>
+          </InputLabel>
+          {!isValidKeskiarvo(keskiarvot?.kaikki) && (
+            <Typography variant="body2" className={classes.error}>
+              {t('pistelaskuri.error.keskiarvo')}
+            </Typography>
+          )}
+        </Grid>
+      </Grid>
+    </LaskuriContainer>
   );
 };
