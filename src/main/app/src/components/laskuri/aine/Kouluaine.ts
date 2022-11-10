a6de8cc791b124129292d440c3de9ae7b4c5f6a9
Risto Salama <risto.salama@gofore.com>
2022-11-10 14:20:54 +0200

    Display all kouluaineet

--- src/main/app/src/components/laskuri/aine/Kouluaine.ts
+++ src/main/app/src/components/laskuri/aine/Kouluaine.ts
@@ -4,3 +4,34 @@ export interface Kouluaine {
   valinnaisetArvosanat: Array<number | null>;
   painokerroin: string;
 }
+
+const createKouluaine = (nimi: string): Kouluaine => {
+  return { nimi, arvosana: null, valinnaisetArvosanat: [], painokerroin: '' };
+};
+
+export class Kouluaineet {
+  kielet: Array<Kouluaine> = [
+    createKouluaine('Äidinkieli ja kirjallisuus'),
+    createKouluaine('A1-kieli'),
+    createKouluaine('B1-Kieli'),
+  ];
+  lisakielet: Array<Kouluaine> = [];
+  muutLukuaineet: Array<Kouluaine> = [
+    createKouluaine('Matematiikka'),
+    createKouluaine('Biologia'),
+    createKouluaine('Maantieto'),
+    createKouluaine('Fysiikka'),
+    createKouluaine('Kemia'),
+    createKouluaine('Terveystieto'),
+    createKouluaine('Uskonto tai elämänkatsomustieto'),
+    createKouluaine('Historia'),
+    createKouluaine('Yhteiskuntaoppi'),
+  ];
+  taitoaineet: Array<Kouluaine> = [
+    createKouluaine('Musiikki'),
+    createKouluaine('Kuvataide'),
+    createKouluaine('Käsityö'),
+    createKouluaine('Liikunta'),
+    createKouluaine('Kotitalous'),
+  ];
+}
