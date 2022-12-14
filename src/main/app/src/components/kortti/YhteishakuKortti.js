d6e7c8361b47f2e5610fd7d0d64f6860c6ccde8b
Mikko Siukola <mikko.siukola@cybercom.com>
2022-12-14 16:13:36 +0200

    OY-3944 Improve styling, add use translations for button texts if available

--- src/main/app/src/components/kortti/YhteishakuKortti.js
+++ src/main/app/src/components/kortti/YhteishakuKortti.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { Grid, Card, CardContent, Paper, Button, Box } from '@mui/material';
+import { Grid, Card, CardContent, Paper, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import clsx from 'clsx';
 import Markdown from 'markdown-to-jsx';
+import { useTranslation } from 'react-i18next';
 
 import { colors } from '#/src/colors';
 import { useContentful } from '#/src/hooks';
@@ -12,27 +13,37 @@ const PREFIX = 'YhteishakuKortti';
 
 const classes = {
   card: `${PREFIX}-card`,
-  media: `${PREFIX}-media`,
-  link: `${PREFIX}-link`,
-  linkElement: `${PREFIX}-linkElement`,
   otsikko: `${PREFIX}-otsikko`,
-  haku: `${PREFIX}-haku`,
-  verkko: `${PREFIX}-verkko`,
-  polku: `${PREFIX}-polku`,
+  kuvaus: `${PREFIX}-kuvaus`,
   toinenaste: `${PREFIX}-toinenaste`,
   kk: `${PREFIX}-kk`,
   button: `${PREFIX}-button`,
+  buttons: `${PREFIX}-buttons`,
   hakulomakeButton: `${PREFIX}-hakulomakebutton`,
   hakulomakeButtonKk: `${PREFIX}-hakulomakebuttonkk`,
 };
 
-const StyledGrid = styled(Grid)({
+const StyledGrid = styled(Grid)(({ theme }) => ({
   [`& .${classes.card}`]: {
     display: 'block',
     height: '100%',
+    textAlign: 'center',
+  },
+  [`& .${classes.otsikko}`]: {
+    width: '80%',
+    marginLeft: '10%',
+    display: 'block',
+    textAlign: 'center',
+  },
+  [`& .${classes.kuvaus}`]: {
+    marginLeft: '5%',
+    [theme.breakpoints.up('xl')]: {
+      marginLeft: '10%',
+      marginRight: '10%',
+    },
+    display: 'block',
   },
   [`& .${classes.toinenaste}`]: {
-    background: colors.brandGreen,
     backgroundColor: colors.brandGreen,
     color: colors.white,
     borderRadius: 2,
@@ -42,23 +53,29 @@ const StyledGrid = styled(Grid)({
     backgroundColor: colors.kkMagenta,
     color: colors.white,
     borderRadius: 2,
-    padding: '25px 20px',
+    padding: '15px 10px',
   },
   [`& .${classes.button}`]: {
     color: colors.white,
     borderColor: colors.white,
     borderRadius: 3,
-    padding: '5px 10px',
-    margin: '5px',
+    padding: '5px 5%',
+    margin: '5px 5px',
     fontSize: '14px',
     fontWeight: 'bold',
+  },
+  [`& .${classes.buttons}`]: {
+    display: 'block',
+    width: 'auto',
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
   [`& .${classes.hakulomakeButton}`]: {
     color: colors.brandGreen,
     borderColor: colors.white,
     backgroundColor: colors.white,
     borderRadius: 3,
-    padding: '5px 10px',
+    padding: '5px 5%',
     margin: '5px',
     fontSize: '16px',
     fontWeight: 'bold',
@@ -68,14 +85,15 @@ const StyledGrid = styled(Grid)({
     borderColor: colors.white,
     backgroundColor: colors.white,
     borderRadius: 3,
-    padding: '5px 10px',
+    padding: '5px 5%',
     margin: '5px',
     fontSize: '16px',
     fontWeight: 'bold',
   },
-});
+}));
 
 const YhteishakuKortti = ({ id, n }) => {
+  const { t } = useTranslation();
   const { data } = useContentful();
   const tdata = data?.infoYhteishaku;
   const yhInfo = tdata[id];
@@ -88,33 +106,39 @@ const YhteishakuKortti = ({ id, n }) => {
     <StyledGrid item xs={12} sm={12} md={12} lg={12} xl={width}>
       <Card className={clsx(classes.card, classes[yhInfo.color])}>
         <CardContent>
+          <h2 className={classes.otsikko}>{yhInfo.otsikko}</h2>
           <Paper className={clsx(classes[yhInfo.color])} elevation={0}>
             <span className="notification-content">
-              <h2>{yhInfo.otsikko}</h2>
-              <Markdown>{yhInfo.kuvaus}</Markdown>
+              <Markdown className={classes.kuvaus}>{yhInfo.kuvaus}</Markdown>
             </span>
           </Paper>
 
-          <Box display="flex" justifyContent="space-between">
+          <Grid item className={classes.buttons}>
             <Button
+              item
+              xs={4}
               className={kk ? classes.hakulomakeButtonKk : classes.hakulomakeButton}
               variant={'outlined'}
               href={yhInfo.linkkiHakulomakkeelle}>
-              {'Täytä hakulomake'}
+              {t('yhteishaku-info.tayta-hakulomake') || 'FIXME Täytä hakulomake'}
             </Button>
             <Button
+              item
+              xs={4}
               className={classes.button}
               variant={'outlined'}
               href={yhInfo.linkkiOhjeisiin}>
-              {'Ohjeet hakemiseen'}
+              {t('yhteishaku-info.ohjeet-hakemiseen') || 'FIXME Ohjeet hakemiseen'}
             </Button>
             <Button
+              item
+              xs={4}
               className={classes.button}
               variant={'outlined'}
               href={yhInfo.linkkiHakutuloksiin}>
-              {'Tutustu koulutuksiin'}
+              {t('yhteishaku-info.tutustu-koulutuksiin') || 'FIXME Tutustu koulutuksiin'}
             </Button>
-          </Box>
+          </Grid>
         </CardContent>
       </Card>
     </StyledGrid>
