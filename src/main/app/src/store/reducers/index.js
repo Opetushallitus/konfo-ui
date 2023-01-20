b896e04c88922f9f4a591942ca90afee1a4d950a
Risto Salama <risto.salama@gofore.com>
2023-01-20 09:57:28 +0200

    Fix import

--- src/main/app/src/store/reducers/index.js
+++ src/main/app/src/store/reducers/index.js
@@ -4,7 +4,7 @@ import appSlice from './appSlice';
 import hakutulosSlice from './hakutulosSlice';
 import koulutusSlice from './koulutusSlice';
 import oppilaitosSlice from './oppilaitosSlice';
-import pistelaskuriSlice from './pistelaskuriSlice';
+import { pistelaskuriSlice } from './pistelaskuriSlice';
 
 export default combineReducers({
   koulutus: koulutusSlice,
