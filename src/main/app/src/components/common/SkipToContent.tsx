5462c4799620a28fb111acea5c0df576464f3353
Marja Kari <marja.kari@amban.fi>
2023-02-15 16:26:09 +0200

    OY-3637 siirry sisältöön komponentti nyt myös lisätty

---
+++ src/main/app/src/components/common/SkipToContent.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Link } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { useTranslation } from 'react-i18next';
+
+import { colors } from '#/src/colors';
+
+const SkipLink = styled(Link)({
+  position: 'absolute',
+  left: '-9999px',
+  zIndex: '999',
+  opacity: '0',
+
+  [`&.Mui-focusVisible`]: {
+    color: colors.black,
+    background: colors.lightGreenBg,
+    height: '30px',
+    left: '50%',
+    textAlign: 'center',
+    padding: '8px',
+    paddingBottom: '30px',
+    position: 'absolute',
+    zIndex: '100000',
+    opacity: '1',
+  },
+});
+
+export const SkipToContent = () => {
+  const { t } = useTranslation();
+  return <SkipLink href="#app-main-content">{t('siirry-sisaltoon')}</SkipLink>;
+};
