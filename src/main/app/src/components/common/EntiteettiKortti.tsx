9c3be36509dce9e7cbe507d0cbc7f833c62a47f8
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-05-19 09:16:30 +0300

    OY-4226: Parannettu EntiteettiKortti-komponentin IconText-tyyppiä

--- src/main/app/src/components/common/EntiteettiKortti.tsx
+++ src/main/app/src/components/common/EntiteettiKortti.tsx
@@ -1,6 +1,14 @@
 import DirectionsOutlinedIcon from '@mui/icons-material/DirectionsOutlined';
 import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
-import { Hidden, Paper, Typography, useMediaQuery, useTheme, Box } from '@mui/material';
+import {
+  Hidden,
+  Paper,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  Box,
+  SvgIcon,
+} from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { truncate, trim, isEmpty, isUndefined } from 'lodash';
 import { useTranslation } from 'react-i18next';
@@ -94,7 +102,7 @@ const StyledLocalizedLink = styled(LocalizedLink, {
 }));
 
 // TODO: Jostain syystä TS:n labeled tuples ei toiminut, e.g. IconComponent: (...props: any) => JSX.Element
-type IconText = [JSX.Element | string, ((...props: any) => JSX.Element) | undefined];
+type IconText = [JSX.Element | string, typeof SvgIcon | undefined];
 
 type StyledLocalizedLinkProps = {
   isSmall?: boolean;
@@ -108,7 +116,7 @@ type Props = {
   header: string;
   erityisopetusHeader?: boolean;
   kuvaus?: string;
-  iconTexts: Array<IconText | undefined | false | any>;
+  iconTexts: Array<IconText | undefined>;
   logoElement?: React.ReactNode;
   teemakuvaElement?: React.ReactNode;
   isSmall?: boolean;
