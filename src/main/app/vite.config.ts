909b2d2fe8e539a674258e79263c906283481c89
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-05-30 12:15:46 +0300

    Vitessä browserslist käyttöön

--- src/main/app/vite.config.ts
+++ src/main/app/vite.config.ts
@@ -3,6 +3,7 @@ import url from 'url';
 
 import { optimizeLodashImports } from '@optimize-lodash/rollup-plugin';
 import react from '@vitejs/plugin-react';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 import { defineConfig, loadEnv } from 'vite';
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
 import svgr from 'vite-plugin-svgr';
@@ -17,6 +18,7 @@ export default defineConfig(({ mode }) => {
     base: '/konfo',
     build: {
       outDir: 'build',
+      target: browserslistToEsbuild(),
     },
     resolve: {
       alias: {
