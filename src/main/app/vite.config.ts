cdf9c3d4b32328cd35a71817c865b9b2f2c18fbd
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-06-09 11:44:48 +0300

    Optimoitu Cypress-testien ajamista ja parannettu bundlen splittausta

--- src/main/app/vite.config.ts
+++ src/main/app/vite.config.ts
@@ -15,28 +15,58 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({ mode }) => {
   const env = Object.assign({}, process.env, loadEnv(mode, process.cwd(), ''));
 
+  // Buildataan cypress-testejä -> karsitaan plugineja yms. hidastamasta
+  const isCypress = process.env.CYPRESS;
+
   return {
     base: '/konfo',
-    build: {
-      outDir: 'build',
-      target: browserslistToEsbuild(),
-    },
+    build: isCypress
+      ? {}
+      : {
+          outDir: 'build',
+          target: browserslistToEsbuild(),
+          rollupOptions: {
+            output: {
+              manualChunks: (id) => {
+                if (id.includes('victory') || id.includes('d3')) {
+                  return 'victory';
+                } else if (id.includes('react-select')) {
+                  return 'react-select';
+                } else if (id.includes('lodash')) {
+                  return 'lodash';
+                } else if (
+                  id.includes('@mui') ||
+                  id.includes('@emotion') ||
+                  id.includes('@popper')
+                ) {
+                  return 'mui';
+                } else if (id.includes('node_modules')) {
+                  return 'vendor-rest';
+                }
+              },
+            },
+          },
+        },
     resolve: {
       alias: {
         '#': path.resolve(__dirname),
       },
     },
     plugins: [
-      react(),
-      checker({
-        typescript: true,
-        eslint: {
-          lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
-        },
-      }),
-      svgr(),
-      optimizeLodashImports(),
-      pluginRewriteAll(),
+      ...(isCypress
+        ? []
+        : [
+            react(),
+            svgr(),
+            pluginRewriteAll(),
+            checker({
+              typescript: true,
+              eslint: {
+                lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+              },
+            }),
+            optimizeLodashImports(),
+          ]),
     ],
     server: {
       port: Number(env.PORT) || 3005,
