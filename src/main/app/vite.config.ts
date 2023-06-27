88d4cc92f3786eb310e9b59208e67fddb651cd0d
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-06-27 13:20:46 +0300

    Karsitaan turhia Viten dev-plugineja kun buildataan prod-versio

--- src/main/app/vite.config.ts
+++ src/main/app/vite.config.ts
@@ -15,49 +15,47 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({ mode }) => {
   const env = Object.assign({}, process.env, loadEnv(mode, process.cwd(), ''));
 
-  // Buildataan testejä -> karsitaan plugineja yms. hidastamasta
-  const isTest = env.VITEST === 'true' || env.CYPRESS;
+  const isDev = env.NODE_ENV === 'development';
 
   return {
     base: '/konfo',
-    build: isTest
-      ? {}
-      : {
-          outDir: 'build',
-          target: browserslistToEsbuild(),
-          rollupOptions: {
-            output: {
-              manualChunks: (id) => {
-                if (id.includes('victory') || id.includes('d3')) {
-                  return 'victory';
-                } else if (id.includes('react-select')) {
-                  return 'react-select';
-                } else if (id.includes('lodash')) {
-                  return 'lodash';
-                } else if (
-                  id.includes('@mui') ||
-                  id.includes('@emotion') ||
-                  id.includes('@popper')
-                ) {
-                  return 'mui';
-                } else if (id.includes('node_modules')) {
-                  return 'vendor-rest';
-                }
-              },
-            },
+    build: {
+      outDir: 'build',
+      target: browserslistToEsbuild(),
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('victory') || id.includes('d3')) {
+              return 'victory';
+            } else if (id.includes('react-select')) {
+              return 'react-select';
+            } else if (id.includes('lodash')) {
+              return 'lodash';
+            } else if (
+              id.includes('@mui') ||
+              id.includes('@emotion') ||
+              id.includes('@popper')
+            ) {
+              return 'mui';
+            } else if (id.includes('node_modules')) {
+              return 'vendor-rest';
+            }
           },
         },
+      },
+    },
     resolve: {
       alias: {
         '#': path.resolve(__dirname),
       },
     },
     plugins: [
-      ...(isTest
-        ? []
-        : [
-            react(),
-            svgr(),
+      react(),
+      svgr(),
+      optimizeLodashImports(),
+      // Tuotanto-buildissa karsitaan devaus-plugineja hidastamasta
+      ...(isDev
+        ? [
             pluginRewriteAll(),
             checker({
               typescript: true,
@@ -65,21 +63,19 @@ export default defineConfig(({ mode }) => {
                 lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
               },
             }),
-            optimizeLodashImports(),
-          ]),
+          ]
+        : []),
     ],
-    server: isTest
-      ? undefined
-      : {
-          port: Number(env.PORT) || 3005,
-          proxy: {
-            '/konfo-backend': {
-              target: env.OPINTOPOLKU_PROXY_URL,
-              changeOrigin: true,
-              secure: false,
-            },
-          },
+    server: {
+      port: Number(env.PORT) || 3005,
+      proxy: {
+        '/konfo-backend': {
+          target: env.OPINTOPOLKU_PROXY_URL,
+          changeOrigin: true,
+          secure: false,
         },
+      },
+    },
     test: {
       environment: 'jsdom',
       globals: true,
