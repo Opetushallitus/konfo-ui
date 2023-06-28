8f0256b4b396b31be99eaee3051d5323edc312b2
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-06-28 13:59:54 +0300

    Kasvatetaan Playwrightin timeoutteja muualla kuin CI:ssä

--- src/main/app/playwright.config.ts
+++ src/main/app/playwright.config.ts
@@ -12,16 +12,16 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './playwright',
   globalTimeout: undefined,
-  timeout: 20 * 1000,
+  timeout: process.env.CI ? 20 * 1000 : 60 * 1000,
   expect: {
-    timeout: 8 * 1000,
+    timeout: process.env.CI ? 8 * 1000 : 15 * 1000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: Boolean(process.env.CI),
   /* Retry on CI only */
-  retries: 1,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
