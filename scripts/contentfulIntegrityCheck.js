f7fdfe688e2d3ca1e87d8fa953dc2d79b3bd23be
Joona Haavisto <joona.haavisto@gofore.com>
2023-09-18 17:54:33 +0300

    Test without set failed

--- scripts/contentfulIntegrityCheck.js
+++ scripts/contentfulIntegrityCheck.js
@@ -85,5 +85,5 @@ const diff = {fi: compareTypes(spaceIdFI), en: compareTypes(spaceIdEN)};
 if (Object.keys(diff.fi).length > 0 || Object.keys(diff.en).length > 0) {
   core.warning('Differences in content types');
   core.info(JSON.stringify(diff, undefined, 2));
-  core.setFailed('Differences in content types');
+  //core.setFailed('Differences in content types');
 }
