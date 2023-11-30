1406e97c8d2b28243922f9470a75ac4ce19b4a06
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2023-11-30 13:09:47 +0200

    OPHJOD-127: Hakutuloksia näytetään max. 5, näytä kaikki -nappi johtaa nyt haku-sivulle

--- src/main/app/src/components/haku/EmbeddedHakutulosBox.tsx
+++ src/main/app/src/components/haku/EmbeddedHakutulosBox.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-
-import { Box, Button, useTheme } from '@mui/material';
-import { omit } from 'lodash';
+import { Box, Button, Typography, useTheme } from '@mui/material';
+import { last, omit } from 'lodash';
 import qs from 'query-string';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from 'react-query';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 
 import { searchKoulutukset } from '#/src/api/konfoApi';
 import { LoadingCircle } from '#/src/components/common/LoadingCircle';
 import { useHakutulosWidth } from '#/src/store/reducers/appSlice';
+import { getLanguage } from '#/src/tools/localization';
 
 import { BackendErrorMessage } from './hakutulos/BackendErrorMessage';
 import {
@@ -17,7 +17,7 @@ import {
 } from './hakutulos/hakutulosKortit/KoulutusKortti';
 import { useSyncHakutulosWidth } from './hakutulos/hooks';
 
-const MAX_SIZE_FOR_INITIAL_QUERY = 10;
+const MAX_SIZE_FOR_INITIAL_QUERY = 5;
 
 type KoulutusQueryResult = {
   total: number;
@@ -31,28 +31,22 @@ type Props = {
 
 const useKoulutusSearch = ({ searchParams, size }: Props) => {
   const keywordAndParams = searchParams.split('?');
+  const baseParams = {
+    ...qs.parse(last(keywordAndParams) || '', { parseNumbers: true }),
+    size,
+    page: 1,
+  };
   const searchParmsWithKeyword =
     keywordAndParams.length > 1
       ? {
-          ...qs.parse(keywordAndParams[1], { parseNumbers: true }),
           keyword: keywordAndParams[0],
+          ...baseParams,
         }
-      : qs.parse(keywordAndParams[0], { parseNumbers: true });
-
-  const completeSearchParams =
-    size && size >= 0
-      ? {
-          ...searchParmsWithKeyword,
-          size,
-          page: 1,
-        }
-      : {
-          ...omit(searchParmsWithKeyword, ['size', 'page']),
-        };
+      : baseParams;
 
   return useQuery<unknown, unknown, KoulutusQueryResult>(
-    ['searchKoulutukset', { search: completeSearchParams }],
-    ({ signal }) => searchKoulutukset(completeSearchParams, signal),
+    ['searchKoulutukset', { search: searchParmsWithKeyword }],
+    ({ signal }) => searchKoulutukset(searchParmsWithKeyword, signal),
     {
       keepPreviousData: false,
       refetchOnMount: false,
@@ -62,9 +56,24 @@ const useKoulutusSearch = ({ searchParams, size }: Props) => {
   );
 };
 
+const navigateToHakupage = ({ searchParams }: Props, navigate: NavigateFunction) => {
+  const keywordAndParams = searchParams.split('?');
+  const params = qs.parse(last(keywordAndParams) || '', { parseNumbers: true });
+  const keyword = keywordAndParams.length > 1 ? keywordAndParams[0] : params['keyword'];
+  const url = `/${getLanguage()}/haku${keyword ? '/' + keyword : ''}?${qs.stringify(
+    omit(params, 'keyword'),
+    {
+      arrayFormat: 'comma',
+    }
+  )}`;
+  navigate(url);
+};
+
 export const EmbeddedHakutulosBox = (props: Props) => {
-  const [size, setSize] = useState<number>(MAX_SIZE_FOR_INITIAL_QUERY);
-  const { data, isLoading, status } = useKoulutusSearch({ ...props, size });
+  const { data, isLoading, status } = useKoulutusSearch({
+    ...props,
+    size: MAX_SIZE_FOR_INITIAL_QUERY,
+  });
 
   const hakutulosRef = useSyncHakutulosWidth();
   const [hakutulosWidth] = useHakutulosWidth();
@@ -72,8 +81,9 @@ export const EmbeddedHakutulosBox = (props: Props) => {
   const theme = useTheme();
   const isSmall = hakutulosWidth < theme.breakpoints.values['sm'];
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
-  const showMoreButton = (data?.total || 0) > MAX_SIZE_FOR_INITIAL_QUERY && size >= 0;
+  const showMoreButton = (data?.total || 0) > MAX_SIZE_FOR_INITIAL_QUERY;
   return (
     <Box
       sx={{ marginTop: `${theme.spacing(2)}`, marginBottom: `${theme.spacing(2)}` }}
@@ -89,25 +99,31 @@ export const EmbeddedHakutulosBox = (props: Props) => {
               isSmall={isSmall}
             />
           ))}
-          <Box
-            display={showMoreButton ? 'flex' : 'none'}
-            justifyContent="center"
-            sx={{ marginTop: `${theme.spacing(3)}` }}>
-            <Button
-              onClick={() => setSize(-1)}
-              type="submit"
-              variant="contained"
-              color="secondary"
-              sx={{
-                display: 'inline-flex',
-                flexShrink: 0,
-                fontSize: '16px',
-                fontWeight: '600',
-                textAlign: 'center',
-              }}>
-              {t('haku.nayta-kaikki-koulutukset')}
-            </Button>
-          </Box>
+          {data?.total === 0 && (
+            <Box display="flex" justifyContent="center">
+              <Typography variant="h1">{t('haku.ei-hakutuloksia')}</Typography>
+            </Box>
+          )}
+          {showMoreButton && (
+            <Box
+              display="flex"
+              justifyContent="center"
+              sx={{ marginTop: `${theme.spacing(3)}` }}>
+              <Button
+                onClick={() => navigateToHakupage(props, navigate)}
+                variant="contained"
+                color="secondary"
+                sx={{
+                  display: 'inline-flex',
+                  flexShrink: 0,
+                  fontSize: '16px',
+                  fontWeight: '600',
+                  textAlign: 'center',
+                }}>
+                {t('haku.nayta-kaikki-koulutukset')}
+              </Button>
+            </Box>
+          )}
         </Box>
       )}
     </Box>
