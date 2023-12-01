849835a5132b71243fd99b32fe0ecb2f74b7c813
Tomi Katajanheimo <tomi@katajanheimo.com>
2023-12-01 14:30:40 +0200

    OK-404 Käytä styled-komponenttia

--- src/main/app/src/components/laskuri/InfoBox.tsx
+++ src/main/app/src/components/laskuri/InfoBox.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-
 import { Box, Hidden, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 import { colors, educationTypeColorCode } from '#/src/colors';
 import { MaterialIcon } from '#/src/components/common/MaterialIcon';
-import { theme } from '#/src/theme';
+import { styled } from '#/src/theme';
 
-const PREFIX = 'InfoBox__';
-
-const classes = {
-  infoBox: `${PREFIX}infobox`,
-  infoIcon: `${PREFIX}__infobox__icon`,
-};
+const InfoIcon = styled(MaterialIcon)(({ theme }) => ({
+  marginRight: '8px',
+  color: colors.brandGreen,
+  [theme.breakpoints.down('sm')]: {
+    marginRight: '4px',
+    verticalAlign: 'text-bottom',
+  },
+}));
 
 export const InfoBox = () => {
   const { t } = useTranslation();
   return (
     <Box
-      className={classes.infoBox}
+      data-testid="infobox"
       sx={{
         maxWidth: '982px',
         display: 'flex',
@@ -29,21 +29,13 @@ export const InfoBox = () => {
         paddingRight: '0.9rem',
         backgroundColor: educationTypeColorCode.ammatillinenGreenBg,
         marginBottom: '1rem',
-        [`.${classes.infoIcon}`]: {
-          marginRight: '8px',
-          color: colors.brandGreen,
-          [theme.breakpoints.down('sm')]: {
-            marginRight: '4px',
-            verticalAlign: 'text-bottom',
-          },
-        },
       }}>
       <Hidden smDown>
-        <MaterialIcon icon="info" variant="outlined" className={classes.infoIcon} />
+        <InfoIcon icon="info" variant="outlined" />
       </Hidden>
       <Typography variant="body1">
         <Hidden smUp>
-          <MaterialIcon icon="info" variant="outlined" className={classes.infoIcon} />
+          <InfoIcon icon="info" variant="outlined" />
         </Hidden>
         {t('pistelaskuri.graafi.info')}
         <span style={{ fontWeight: 600 }}>
