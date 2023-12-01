24aad7a053f2ef649eb2b73e0c868ce3c2f0de1a
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-12-01 16:04:30 +0200

    OK-376: Parannettu entiteetin lyhennetyn kuvauksen muodostamista

--- src/main/app/src/hooks/useTruncatedKuvaus.test.ts
+++ src/main/app/src/hooks/useTruncatedKuvaus.test.ts
@@ -12,6 +12,16 @@ describe('stripKuvausHTML', () => {
       'Oppilas osaa lukea, kirjoittaa, puhua.',
       undefined,
     ],
+    [
+      '<p><strong>PÄÄOTSIKKO</strong></p><p>Ensimmäinen kappale.</p><p></p><p><strong>Alaotsikko</strong></p><p>Toinen kappale</p>',
+      'PÄÄOTSIKKO: Ensimmäinen kappale. Alaotsikko: Toinen kappale',
+      undefined,
+    ],
+    [
+      '<h3>PÄÄOTSIKKO</h3><p>Ensimmäinen kappale.</p><p></p><h3>Alaotsikko</h3><p>Toinen kappale</p>',
+      'PÄÄOTSIKKO: Ensimmäinen kappale. Alaotsikko: Toinen kappale',
+      undefined,
+    ],
   ])('stripKuvausHTML should work right', (kuvaus, stripped, limit) => {
     expect(stripKuvausHTML(kuvaus, limit)).toEqual(stripped);
   });
--- src/main/app/src/hooks/useTruncatedKuvaus.tsx
+++ src/main/app/src/hooks/useTruncatedKuvaus.tsx
@@ -9,6 +9,8 @@ export const stripKuvausHTML = (kuvaus: string, limit?: number) => {
   const slicedKuvaus = limit ? kuvaus.slice(0, limit) : kuvaus;
   return stripTags(
     slicedKuvaus
+      .replace(/<\/strong><\/p>|<\/h\d>/g, ': ')
+      .replace(/\.<\/p>/g, '. ')
       .replace(/<ul>|<ol>|(<\/?[a-zA-Z]{0,10}$)/gm, ' ')
       .replace(/([^.])<\/li>/gm, '$1, ')
       .trim()
