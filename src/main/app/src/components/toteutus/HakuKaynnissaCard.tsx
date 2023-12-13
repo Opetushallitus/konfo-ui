807f14500431c7f0726cd1cf012ae5712e2f3e52
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-12-13 15:03:26 +0200

    Siistitty HakuKaynnissaCard tyylimäärittelyt

--- src/main/app/src/components/toteutus/HakuKaynnissaCard.tsx
+++ src/main/app/src/components/toteutus/HakuKaynnissaCard.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-
 import { Button, Card, CardActions, CardContent, Grid, Typography } from '@mui/material';
 
 import { styled } from '#/src/theme';
-
-const PREFIX = 'HakuKaynnissaCard';
-
-const classes = {
-  card: `${PREFIX}-card`,
-  gridWrapper: `${PREFIX}-gridWrapper`,
-  buttonText: `${PREFIX}-buttonText`,
-};
 
 const StyledCard = styled(Card)({
   marginTop: '40px',
   padding: '20px',
   display: 'flex',
   flexWrap: 'wrap',
-  [`& .${classes.buttonText}`]: { color: '#FFFFFF' },
 });
 
 type Props = {
@@ -29,8 +18,8 @@ type Props = {
 
 export const HakuKaynnissaCard = ({ title, text, href, buttonText }: Props) => {
   return (
-    <StyledCard className={classes.card} elevation={2}>
-      <CardContent className={classes.gridWrapper}>
+    <StyledCard elevation={2}>
+      <CardContent>
         <Grid container alignItems="center" justifyContent="space-between">
           <Grid item>
             <Typography variant="h4" gutterBottom>
@@ -42,9 +31,7 @@ export const HakuKaynnissaCard = ({ title, text, href, buttonText }: Props) => {
       </CardContent>
       <CardActions>
         <Button href={href} variant="contained" size="large" color="primary">
-          <Typography className={classes.buttonText} variant="body1">
-            {buttonText}
-          </Typography>
+          {buttonText}
         </Button>
       </CardActions>
     </StyledCard>
