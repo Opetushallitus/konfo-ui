370638a6552035457d42cdc74a97e5518f8a6621
August Kilponen <august.kilponen@gmail.com>
2023-12-13 14:42:25 +0200

    Lisätty Askem-palautekomponentti tarvittaville sivuille

---
+++ src/main/app/src/components/common/Askem.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+
+import { getLanguage } from '#/src/tools/localization';
+
+declare const window: Window &
+  typeof globalThis & {
+    askem: any;
+  };
+
+const getApiKey = () => {
+  const language = getLanguage();
+  console.log('Askem - language: ' + language);
+  switch (language) {
+    case 'en':
+      return 'idze486kova0hcrl';
+    case 'sv':
+      return 'idze486kova0hcrl';
+    default:
+      return 'idze486kova0hcrl';
+  }
+};
+
+const hostnames: Array<string> = [
+  'opintopolku.fi',
+  'testiopintopolku.fi',
+  'untuvaopintopolku.fi',
+  'hahtuvaopintopolku.fi',
+  'localhost',
+];
+const enableAskem = (): boolean => {
+  const hostname = window.location.hostname;
+  console.log('Askem - hostname: ' + hostname);
+  return hostnames.includes(hostname);
+};
+
+export const Askem = () => {
+  useEffect(() => {
+    window.askem = {
+      settings: {
+        apiKey: getApiKey(),
+      },
+    };
+
+    const script = document.createElement('script');
+
+    script.src = 'https://cdn.askem.com/plugin/askem.js';
+    script.async = true;
+
+    if (enableAskem()) document.body.appendChild(script);
+
+    return () => {
+      if (enableAskem()) document.body.removeChild(script);
+    };
+  }, []);
+
+  return <div className="askem"></div>;
+};
