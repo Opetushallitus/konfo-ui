ce2e3c573720c11c75dbf37409f5cd2a9092c37f
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-12-15 13:28:42 +0200

    Siivottu pois käyttämättömät tyylit KoulutustyyppiSuodatin-komponetista

--- src/main/app/src/components/suodattimet/hakutulosSuodattimet/KoulutustyyppiSuodatin.tsx
+++ src/main/app/src/components/suodattimet/hakutulosSuodattimet/KoulutustyyppiSuodatin.tsx
@@ -1,47 +1,11 @@
-import React from 'react';
-
 import { useTranslation } from 'react-i18next';
 
-import { colors } from '#/src/colors';
 import { Filter } from '#/src/components/common/Filter';
 import { RAJAIN_TYPES } from '#/src/constants';
-import { styled } from '#/src/theme';
 import { useRajainItems } from '#/src/tools/filters';
 import { RajainComponentProps } from '#/src/types/SuodatinTypes';
 
 import { useCheckboxRajainOnChange } from '../common/useCheckboxRajainOnChange';
-
-const classes = {
-  noBoxShadow: 'noBoxShadow',
-  buttonRoot: 'buttonRoot',
-  buttonLabelTruncated: 'buttonLabelTruncated',
-  buttonActive: 'buttonActive',
-  buttonInactive: 'buttonInactive',
-};
-
-const Root = styled('div')({
-  [`& .${classes.buttonRoot}`]: {
-    fontSize: 14,
-    fontWeight: 600,
-    padding: '5px',
-  },
-  [`& .${classes.buttonLabelTruncated}`]: {
-    overflow: 'hidden',
-    display: 'block',
-    textOverflow: 'ellipsis',
-  },
-  [`& .${classes.buttonActive}`]: {
-    backgroundColor: colors.brandGreen,
-    color: colors.white,
-    '&:hover': {
-      backgroundColor: colors.brandGreen,
-    },
-  },
-  [`& .${classes.buttonInactive}`]: {
-    backgroundColor: colors.white,
-    color: colors.brandGreen,
-  },
-});
 
 export const KoulutustyyppiSuodatin = (props: RajainComponentProps) => {
   const { t } = useTranslation();
@@ -57,15 +21,13 @@ export const KoulutustyyppiSuodatin = (props: RajainComponentProps) => {
   const onItemChange = useCheckboxRajainOnChange(rajainItems, setRajainValues);
 
   return (
-    <Root>
-      <Filter
-        {...props}
-        testId="koulutustyyppi-filter"
-        name={t('haku.koulutustyyppi')}
-        rajainItems={rajainItems}
-        onItemChange={onItemChange}
-        displaySelected
-      />
-    </Root>
+    <Filter
+      {...props}
+      testId="koulutustyyppi-filter"
+      name={t('haku.koulutustyyppi')}
+      rajainItems={rajainItems}
+      onItemChange={onItemChange}
+      displaySelected
+    />
   );
 };
