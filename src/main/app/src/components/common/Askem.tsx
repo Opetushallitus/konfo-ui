1ff9203cc6f40323ecd7547a9ed2c0c85b62f191
Risto Salama <risto.salama@gofore.com>
2023-12-19 11:53:56 +0200

    Make askem fit page better

--- src/main/app/src/components/common/Askem.tsx
+++ src/main/app/src/components/common/Askem.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 
+import { styled } from '#/src/theme';
 import { getLanguage } from '#/src/tools/localization';
 
 declare const window: Window &
@@ -47,5 +48,48 @@ export const Askem = () => {
     };
   }, []);
 
-  return <div className="askem"></div>;
+  const buttonStyles = {
+    textTransform: 'none',
+    transition:
+      'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+    fontSize: '16px',
+    fontWeight: 600,
+    boxShadow:
+      '0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)',
+    borderRadius: '4px',
+    lineHeight: '1.75',
+    padding: '8px 16px',
+    '&:hover': {
+      backgroundColor: 'rgb(40, 85, 11)',
+      top: 0,
+      left: 0,
+    },
+  };
+
+  const StyledAskem = styled('div')(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'left',
+    width: '80%',
+    [theme.breakpoints.down('sm')]: {
+      width: '95%',
+    },
+    '.askem-plugin': {
+      '.askem-header-text': {
+        fontSize: '2rem',
+        fontWeight: 700,
+      },
+      '.askem-reactions-group': {
+        '.askem-reaction-button': buttonStyles,
+      },
+      '.askem-inputs': {
+        '.askem-form-submit': buttonStyles,
+        '.askem-form-submit:not(.disabled)': {
+          top: '0 !important',
+          left: 'inherit',
+        },
+      },
+    },
+  }));
+
+  return <StyledAskem className="askem"></StyledAskem>;
 };
