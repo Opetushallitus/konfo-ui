21d43e0e547523ab5a5b00ab6f263f496d2941b7
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-01-03 16:47:29 +0200

    Siistitty IconBackground tyylit

--- src/main/app/src/components/common/IconBackground.tsx
+++ src/main/app/src/components/common/IconBackground.tsx
@@ -1,28 +1,10 @@
-import React from 'react';
-
 import { colors } from '#/src/colors';
 import { styled } from '#/src/theme';
 
-const PREFIX = 'IconBackground';
-
-const classes = {
-  iconBackground: `${PREFIX}-iconBackground`,
-};
-
-const Root = styled('span')<Props>(({ color }) => ({
+export const IconBackground = styled('span')<{
+  color?: string;
+}>(({ color }) => ({
   borderRadius: '50%',
   backgroundColor: color ?? colors.brandGreen,
   padding: '12.5px 15px', // TODO: forced square ratio would be nice but prolly very hard to implement
 }));
-
-type Props = React.PropsWithChildren<{
-  color?: string;
-}>;
-
-export const IconBackground = ({ children, color }: Props) => {
-  return (
-    <Root className={classes.iconBackground} color={color}>
-      {children}
-    </Root>
-  );
-};
