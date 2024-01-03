809cd4404d89fc2f29b8fac65b68203672f28230
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-01-03 17:07:56 +0200

    OK-377: Lisätty yleiskäyttöinen KonfoDialog-komponentti ja otettu se käyttään ConfirmSuosikkiRemoveDialog ja SiirryHakulomakkeelleDialog komponenteissa

--- src/main/app/src/components/SuosikitVertailuPage/SiirryHakulomakkeelleDialog.tsx
+++ src/main/app/src/components/SuosikitVertailuPage/SiirryHakulomakkeelleDialog.tsx
@@ -1,12 +1,12 @@
-import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { create } from 'zustand';
 
+import { ExternalLinkButton } from '#/src/components/common/ExternalLinkButton';
+import { KonfoDialog } from '#/src/components/common/KonfoDialog';
 import { TextButton } from '#/src/components/common/TextButton';
 import { VertailuSuosikki } from '#/src/types/common';
 
 import { useSiirryHakulomakkeelleInfo } from './useSiirryHakulomakkeelleInfo';
-import { ExternalLinkButton } from '../common/ExternalLinkButton';
 
 interface DialogState {
   isOpen: boolean;
@@ -34,34 +34,21 @@ export const SiirryHakulomakkeelleDialog = ({
   const { t } = useTranslation();
   const { url } = useSiirryHakulomakkeelleInfo(data);
   return (
-    <Dialog
+    <KonfoDialog
       fullWidth={true}
       maxWidth="md"
       onClose={onClose}
       open={open}
-      sx={{
-        '& .MuiPaper-root': {
-          padding: (theme) => theme.spacing(1),
-        },
-      }}>
-      <DialogTitle variant="h1" sx={{ margin: 0 }}>
-        {t('suosikit-vertailu.siirry-hakulomakkeelle-dialog-title')}
-      </DialogTitle>
-      <DialogContent>
-        {t('suosikit-vertailu.siirry-hakulomakkeelle-dialog-text')}
-      </DialogContent>
-      <DialogActions
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          gap: 2,
-          margin: (theme) => theme.spacing(1),
-        }}>
-        <ExternalLinkButton href={url} onClick={onClose}>
-          {t('suosikit-vertailu.siirry-hakulomakkeelle')}
-        </ExternalLinkButton>
-        <TextButton onClick={() => onClose()}>{t('peruuta')}</TextButton>
-      </DialogActions>
-    </Dialog>
+      title={t('suosikit-vertailu.siirry-hakulomakkeelle-dialog-title')}
+      content={t('suosikit-vertailu.siirry-hakulomakkeelle-dialog-text')}
+      actions={
+        <>
+          <ExternalLinkButton href={url} onClick={onClose}>
+            {t('suosikit-vertailu.siirry-hakulomakkeelle')}
+          </ExternalLinkButton>
+          <TextButton onClick={() => onClose()}>{t('peruuta')}</TextButton>
+        </>
+      }
+    />
   );
 };
