e781a3a4c710f763036428514ac95cc2d3dd5f43
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-01-04 14:48:30 +0200

    Siistitty ContentWithTopIcon-komponentin tyylit

--- src/main/app/src/components/common/ContentWithTopIcon.tsx
+++ src/main/app/src/components/common/ContentWithTopIcon.tsx
@@ -1,17 +1,7 @@
-import React from 'react';
-
 import { Avatar, Box } from '@mui/material';
 
 import { colors, educationTypeColorCode } from '#/src/colors';
 import { styled } from '#/src/theme';
-
-const PREFIX = 'ContentWithTopIcon';
-
-const classes = {
-  container: `${PREFIX}-container`,
-  box: `${PREFIX}-box`,
-  avatar: `${PREFIX}-avatar`,
-};
 
 const StyledBox = styled(Box)(({ theme }) => ({
   marginTop: theme.spacing(6),
@@ -20,27 +10,27 @@ const StyledBox = styled(Box)(({ theme }) => ({
   },
   display: 'flex',
   justifyContent: 'center',
+}));
 
-  [`& .${classes.box}`]: {
-    textAlign: 'center',
-    padding: theme.spacing(6, 12, 6, 12),
-    backgroundColor: educationTypeColorCode.ammatillinenGreenBg,
-    [theme.breakpoints.down('sm')]: {
-      padding: theme.spacing(4, 3, 3, 3),
-    },
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  position: 'absolute',
+  width: theme.spacing(8),
+  height: theme.spacing(8),
+  marginTop: `calc(-${theme.spacing(8)} / 2)`,
+  backgroundColor: colors.brandGreen,
+  [theme.breakpoints.down('sm')]: {
+    width: theme.spacing(6),
+    height: theme.spacing(6),
+    marginTop: `calc(-${theme.spacing(6)} / 2)`,
   },
+}));
 
-  [`& .${classes.avatar}`]: {
-    position: 'absolute',
-    width: theme.spacing(8),
-    height: theme.spacing(8),
-    marginTop: `calc(-${theme.spacing(8)} / 2)`,
-    backgroundColor: colors.brandGreen,
-    [theme.breakpoints.down('sm')]: {
-      width: theme.spacing(6),
-      height: theme.spacing(6),
-      marginTop: `calc(-${theme.spacing(6)} / 2)`,
-    },
+const StyledContent = styled(Box)(({ theme }) => ({
+  textAlign: 'center',
+  padding: theme.spacing(6, 12, 6, 12),
+  backgroundColor: educationTypeColorCode.ammatillinenGreenBg,
+  [theme.breakpoints.down('sm')]: {
+    padding: theme.spacing(4, 3, 3, 3),
   },
 }));
 
@@ -50,9 +40,9 @@ type Props = React.PropsWithChildren<{
 
 export const ContentWithTopIcon = ({ children, icon }: Props) => {
   return (
-    <StyledBox className={classes.container}>
-      <Avatar className={classes.avatar}>{icon}</Avatar>
-      <Box className={classes.box}>{children}</Box>
+    <StyledBox>
+      <StyledAvatar>{icon}</StyledAvatar>
+      <StyledContent>{children}</StyledContent>
     </StyledBox>
   );
 };
