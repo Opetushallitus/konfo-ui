6f7f1de8e1d01079aee19998b9d226b67f691504
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-01-04 14:09:07 +0200

    Siistitty ErrorMessage-komponentin tyylit

--- src/main/app/src/components/common/ErrorMessage.tsx
+++ src/main/app/src/components/common/ErrorMessage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { Avatar, Button, Grid, Paper, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
@@ -7,41 +5,15 @@ import { colors } from '#/src/colors';
 import { MaterialIcon } from '#/src/components/common/MaterialIcon';
 import { styled } from '#/src/theme';
 
-const PREFIX = 'ErrorMessage';
-
-const classes = {
-  gridOuterContainerRoot: `${PREFIX}-gridOuterContainerRoot`,
-  paperRoot: `${PREFIX}-paperRoot`,
-  avatarRoot: `${PREFIX}-avatarRoot`,
-  buttonRoot: `${PREFIX}-buttonRoot`,
-};
-
-const StyledGrid = styled(Grid)(({ theme }) => ({
-  [`& .${classes.gridOuterContainerRoot}`]: {
-    paddingTop: theme.spacing(5),
-  },
-
-  [`& .${classes.paperRoot}`]: {
-    width: '80%',
-    borderTop: `5px solid ${colors.red}`,
-    boxShadow: '0 0 8px 0 rgba(0,0,0,0.2)',
-    margin: 'auto',
-    padding: theme.spacing(5),
-    [theme.breakpoints.down('xs')]: {
-      padding: theme.spacing(3),
-      width: '90%',
-    },
-  },
-
-  [`& .${classes.avatarRoot}`]: {
-    backgroundColor: colors.red,
-    width: 60,
-    height: 60,
-  },
-
-  [`& .${classes.buttonRoot}`]: {
-    fontWeight: 600,
-    fontSize: 16,
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  width: '80%',
+  borderTop: `5px solid ${colors.red}`,
+  boxShadow: '0 0 8px 0 rgba(0,0,0,0.2)',
+  margin: 'auto',
+  padding: theme.spacing(5),
+  [theme.breakpoints.down('xs')]: {
+    padding: theme.spacing(3),
+    width: '90%',
   },
 }));
 
@@ -49,17 +21,17 @@ export const ErrorMessage = ({ onRetry = () => {} }: { onRetry?: () => void }) =
   const { t } = useTranslation();
 
   return (
-    <StyledGrid
-      container
-      classes={{ root: classes.gridOuterContainerRoot }}
-      spacing={4}
-      alignItems="center"
-      direction="column">
-      <Grid item style={{ width: '100%' }}>
-        <Paper classes={{ root: classes.paperRoot }}>
+    <Grid container paddingTop={5} spacing={4} alignItems="center" direction="column">
+      <Grid item sx={{ width: '100%' }}>
+        <StyledPaper>
           <Grid container spacing={4} direction="column" alignItems="center">
             <Grid item>
-              <Avatar classes={{ root: classes.avatarRoot }}>
+              <Avatar
+                sx={{
+                  backgroundColor: colors.red,
+                  width: 60,
+                  height: 60,
+                }}>
                 <MaterialIcon icon="error_outline" />
               </Avatar>
             </Grid>
@@ -73,7 +45,10 @@ export const ErrorMessage = ({ onRetry = () => {} }: { onRetry?: () => void }) =
             </Grid>
             <Grid item>
               <Button
-                classes={{ root: classes.buttonRoot }}
+                sx={{
+                  fontWeight: 600,
+                  fontSize: 16,
+                }}
                 variant="contained"
                 color="secondary"
                 onKeyDown={(event) => event.key === 'Enter' && onRetry()}
@@ -82,8 +57,8 @@ export const ErrorMessage = ({ onRetry = () => {} }: { onRetry?: () => void }) =
               </Button>
             </Grid>
           </Grid>
-        </Paper>
+        </StyledPaper>
       </Grid>
-    </StyledGrid>
+    </Grid>
   );
 };
