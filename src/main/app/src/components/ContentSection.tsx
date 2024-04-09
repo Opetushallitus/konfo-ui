5cac9848c7147db2edda09f9bea137b973333033
Dmitry Marokhonov <dmitry.marokhonov@knowit.fi>
2024-04-09 13:44:46 +0300

    OY-4758: katselmointikorjaukset

--- src/main/app/src/components/ContentSection.tsx
+++ src/main/app/src/components/ContentSection.tsx
@@ -9,15 +9,13 @@ export const ContentSection = ({
   heading,
   children,
   variant = 'h1',
-  hasContent = true,
 }: {
   heading?: string;
   variant?: TypographyVariant;
-  hasContent?: boolean;
 } & BoxProps) => {
   return (
     <WithSideMargins>
-      {heading && children && hasContent ? (
+      {heading && children ? (
         <>
           <Heading variant={variant}>{heading}</Heading>
           <HeadingBoundary>{children}</HeadingBoundary>
