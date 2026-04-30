1801e1993948b3f3df8c35958532059463e50a4e
Dmitry Marokhonov <dmitry.marokhonov@knowit.fi>
2026-04-30 14:59:34 +0300

    OY-4400: Saavutettavuus, Linkit näkymään oikein (avautuuko uuteen välilehteen) avustavalle teknologialle (VoiceOver tai NVDA)

--- src/main/app/src/components/common/ExternalLink.tsx
+++ src/main/app/src/components/common/ExternalLink.tsx
@@ -1,17 +1,25 @@
 import { Link, LinkProps } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
+import { AccessibleInvisibleText } from '#/src/components/common/accessibility/AccessibleInvisibleText';
 import { MaterialIcon } from '#/src/components/common/MaterialIcon';
 
-export const ExternalLink = ({ children, ...props }: LinkProps) => (
-  <Link target="_blank" rel="noopener" variant="body1" {...props}>
-    {children}
-    <MaterialIcon
-      icon="open_in_new"
-      sx={{
-        verticalAlign: 'middle',
-        marginLeft: '5px',
-        marginBottom: '1px',
-      }}
-    />
-  </Link>
-);
+export const ExternalLink = ({ children, ...props }: LinkProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <Link target="_blank" rel="noopener" variant="body1" {...props}>
+      {children}
+      <AccessibleInvisibleText text={` (${t('avautuu-uuteen-valilehteen')})`} />
+      <MaterialIcon
+        icon="open_in_new"
+        aria-hidden="true"
+        sx={{
+          verticalAlign: 'middle',
+          marginLeft: '5px',
+          marginBottom: '1px',
+        }}
+      />
+    </Link>
+  );
+};
