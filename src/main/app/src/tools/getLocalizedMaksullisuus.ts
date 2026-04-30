6d437631042a6e0814c52b46fe09bdb089493a7b
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2026-04-30 13:15:10 +0300

    OY-5501: Päivitetään getLocalizedMaksullisuus käyttämään kahta maksullisuustyyppiä + testit

---
+++ src/main/app/src/tools/getLocalizedMaksullisuus.ts
@@ -0,0 +1,60 @@
+import { isEmpty, some } from 'lodash';
+
+import { KOULUTUS_TYYPPI, Koulutustyyppi, MAKSULLISUUSTYYPPI } from '#/src/constants';
+import { Maksullisuustyyppi } from '#/src/types/ToteutusTypes';
+
+import { getTranslationForKey } from './localization';
+
+export const getLocalizedMaksullisuus = (
+  koulutustyyppi?: Koulutustyyppi,
+  maksullisuustyypit?: Array<Maksullisuustyyppi>,
+  maksuAmount?: number,
+  lukuvuosimaksuAmount?: number
+) => {
+  const isMaksuton = some(
+    maksullisuustyypit,
+    (maksullisuustyyppi: Maksullisuustyyppi): boolean =>
+      maksullisuustyyppi === MAKSULLISUUSTYYPPI.MAKSUTON
+  );
+  const isMaksullinen = some(
+    maksullisuustyypit,
+    (maksullisuustyyppi: Maksullisuustyyppi): boolean =>
+      maksullisuustyyppi === MAKSULLISUUSTYYPPI.MAKSULLINEN
+  );
+  const isLukuvuosimaksullinen = some(
+    maksullisuustyypit,
+    (maksullisuustyyppi: Maksullisuustyyppi): boolean =>
+      maksullisuustyyppi === MAKSULLISUUSTYYPPI.LUKUVUOSIMAKSU
+  );
+
+  if (isEmpty(maksullisuustyypit)) {
+    return getTranslationForKey('toteutus.maksuton');
+  } else {
+    if (isMaksuton) {
+      return getTranslationForKey('toteutus.maksuton');
+    } else if (
+      isLukuvuosimaksullinen &&
+      isMaksullinen &&
+      maksuAmount &&
+      lukuvuosimaksuAmount
+    ) {
+      const maksullinenStr = `${getTranslationForKey('toteutus.maksullinen-opetus')}: ${maksuAmount} €`;
+      const lukuvuosimaksuStr = `${getTranslationForKey('toteutus.lukuvuosimaksu')}: ${lukuvuosimaksuAmount} €`;
+      return `${maksullinenStr}\n${lukuvuosimaksuStr}`;
+    } else if (
+      [KOULUTUS_TYYPPI.AMKKOULUTUS, KOULUTUS_TYYPPI.YLIOPISTOKOULUTUS].includes(
+        koulutustyyppi as Koulutustyyppi
+      ) &&
+      isLukuvuosimaksullinen &&
+      lukuvuosimaksuAmount
+    ) {
+      return `${getTranslationForKey('toteutus.lukuvuosimaksu-kk')}: ${lukuvuosimaksuAmount}`;
+    } else if (isMaksullinen && maksuAmount) {
+      return maksuAmount;
+    } else if (isLukuvuosimaksullinen && lukuvuosimaksuAmount) {
+      return lukuvuosimaksuAmount;
+    } else {
+      return '';
+    }
+  }
+};
